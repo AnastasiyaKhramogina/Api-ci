07bc346d13f5adeecf5b0d41578273d7d12a3ace
Anastasya Khramogina <akhramogina@mail.ru>
2023-05-21 21:47:47 +0300

    first commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -9,7 +9,6 @@ class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
       // Given - When - Then
-      // Предусловия
       given()
           .baseUri("http://localhost:9999/api/v1")
       // Выполняемые действия
